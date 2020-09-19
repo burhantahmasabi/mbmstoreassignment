eb7f1fdbd43e6c328163dcb0b2578c7fda773e9d
burhantahmasabi <burhant@hotmail.com>
2020-09-19 12:13:31 +0200

    added missing files

---
+++ MbmStore/Models/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class Invoice
+    {
+        private decimal totalPrice;
+        public int InvoiceId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; private set; }
+
+        public Customer Customer { get; set; }
+
+        public List<OrderItem> OrderItems { get; private set; } = new List<OrderItem>();
+
+
+
+        public Invoice(int invoiceId, DateTime orderDate, Customer customer)
+        {
+            InvoiceId = invoiceId;
+            OrderDate = orderDate;
+            Customer = customer;
+        }
+
+
+        public void AddOrderItem(Product product, int quantity)
+        {
+
+            OrderItems.Add(new OrderItem(product, quantity));
+        }
+
+    }
+}
---
+++ MbmStore/Models/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class Track
+    {
+        public string Title { get; set; }
+        public string Composer { get; set; }
+        public TimeSpan Length { get; set; }
+
+        public Track() { }
+        public Track(string title) {
+           Title = title;
+            //defaults, change later:
+            Composer = "DefaultComposer";
+            Length = new TimeSpan(0, 3, 18);
+        }
+
+    }
+}
